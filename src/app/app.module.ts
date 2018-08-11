94f65fa766a26e8a1d6920c2186bf63875238436
Sherif Ahmed <sharko.designer@gmail.com>
2018-08-11 15:07:19 +0200

    create auth with signin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+
+
+@NgModule({
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+})
+
+export class MaterialModule {}
