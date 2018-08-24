18832d76df81b8429a7a094047cc8d62db60ad7b
Sherif Ahmed <sharko.designer@gmail.com>
2018-08-24 16:14:49 +0200

    finish core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core.module';
 import { CoursesModule } from './core/courses/courses.module';
 import { StudentsModule } from './core/students/students.module';
 
 import { AppComponent } from './app.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { DashboardComponent } from './core/dashboard/dashboard.component';
-import { ToolbarComponent } from './shared/toolbar/toolbar.component';
-import { SidenavListComponent } from './shared/sidenav-list/sidenav-list.component';
-import { OverviewComponent } from './core/overview/overview.component';
 
 import { AuthService } from './auth/auth.service';
 import { StudentService } from './core/students/student.service';
 import { AppRoutingModule } from './app-routing.module';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { SidenavListComponent } from './shared/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     ToolbarComponent,
     SidenavListComponent,
-    OverviewComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +41,6 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
   ],
   entryComponents: [
-
   ],
   providers: [AuthService, StudentService],
   bootstrap: [AppComponent]
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -5,19 +5,27 @@ import { FormsModule } from "@angular/forms";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from "./material.module";
 import { CoreRoutingModule } from "./core-routing.module";
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { OverviewComponent } from "./core/overview/overview.component";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { AuthService } from "./auth/auth.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
+    NgxDatatableModule,
     CoreRoutingModule,
-
   ],
   declarations: [
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    OverviewComponent,
   ],
-  providers: [],
+  providers: [AuthService],
 })
 export class CoreModule {}
