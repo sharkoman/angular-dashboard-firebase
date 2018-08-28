b8c398c8ba8c5f7960115d3ce1ceccc1308f19a2
Sherif Ahmed <sharko.designer@gmail.com>
2018-08-28 10:32:14 +0200

    change toolbar title

--- src/app/core/students/students.module.ts
+++ src/app/core/students/students.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './../../material.module';
